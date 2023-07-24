c17dd69c025445efa6cf26a6724e244b96d5e4eb
Brice G <bricegoudalo@gmail.com>
2023-07-24 10:42:14 +0200

    Display User list

--- resources/js/components/users/UsersList.vue
+++ resources/js/components/users/UsersList.vue
@@ -1,8 +1,28 @@
-<script type="module">
+<script setup>
+import { ref } from "vue";
+
+let users = ref([]);
+
+const getUsers = () => {
+  axios
+    .get("api/users")
+    .then((res) => {
+      console.log(res.data.data);
+      users.value = res.data.data;
+      console.log("users.value : ", users.value);
+    })
+    .catch((err) => console.log(err));
+};
+
+getUsers();
 </script>
 
 <template>
-  <h1>Hello World</h1>
+  <div>
+    <div v-for="user in users" :key="user.id">
+      <p>{{ user.name }}</p>
+    </div>
+  </div>
 </template>
 
 <style scoped>
---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <h2>Liste des utilisateurs</h2>
+        <users-list />
+    </div>
+</div>
+@endsection
